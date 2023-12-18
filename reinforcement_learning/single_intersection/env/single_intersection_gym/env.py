4a0d76bacaf9fe82e30c2c614cb9db9c19ca62d0
Jeroen van Riel <jeroenvanriel@outlook.com>
2023-12-18 16:02:11 +0100

    remove env rendering code

--- reinforcement_learning/single_intersection/env/single_intersection_gym/env.py
+++ reinforcement_learning/single_intersection/env/single_intersection_gym/env.py
@@ -3,15 +3,8 @@ import pygame
 import numpy as np
 
 class SingleIntersectionEnv(gym.Env):
-    metadata = { "render_modes": ["human", "rgb_array"], "render_fps": 4 }
 
-    def __init__(self, n_lanes=2, n_arrivals=30, horizon=10, switch_over=2, platoon_generator=None, render_mode=None):
-        assert render_mode is None or render_mode in self.metadata["render_modes"]
-        self.render_mode = render_mode
-        self.window_size = (1024, 256)
-        self.window = None
-        self.clock = None
-
+    def __init__(self, n_lanes=2, n_arrivals=30, horizon=10, switch_over=2, platoon_generator=None):
         self.n_lanes = n_lanes
         self.n_arrivals = n_arrivals
         self.horizon = horizon
@@ -48,9 +41,6 @@ class SingleIntersectionEnv(gym.Env):
 
         observation = self._get_obs()
         info = self._get_info()
-
-        if self.render_mode == "human":
-            self._render_frame()
 
         return observation, info
 
@@ -112,9 +102,6 @@ class SingleIntersectionEnv(gym.Env):
         if terminated:
             print("terminated")
 
-        if self.render_mode == "human":
-            self._render_frame()
-
         return observation, reward, terminated, False, info
 
     def _get_obs(self):
@@ -136,82 +123,3 @@ class SingleIntersectionEnv(gym.Env):
 
     def _get_info(self):
         return { "action_sequence": self.action_sequence, "lane_sequence": self.lane_sequence, "platoons_scheduled": self.platoons_scheduled }
-
-    def render(self):
-        if self.render_mode == "rgb_array":
-            return self._render_frame()
-
-    def _render_frame(self):
-        if self.window is None and self.render_mode == "human":
-            pygame.init()
-            pygame.display.init()
-            self.window = pygame.display.set_mode(self.window_size)
-
-        if self.clock is None and self.render_mode == "human":
-            self.clock = pygame.time.Clock()
-
-        canvas = pygame.Surface(self.window_size)
-        canvas.fill((255, 255, 255))
-
-        self._render_timeline(canvas)
-        self._render_vehicles(canvas)
-
-        if self.render_mode == "human":
-            # The following line copies our drawings from `canvas` to the visible window
-            self.window.blit(canvas, canvas.get_rect())
-            pygame.event.pump()
-            pygame.display.update()
-
-            # We need to ensure that human-rendering occurs at the predefined framerate.
-            # The following line will automatically add a delay to keep the framerate stable.
-            self.clock.tick(self.metadata["render_fps"])
-        else:  # rgb_array
-            return np.transpose(
-                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
-            )
-
-    def _render_timeline(self, canvas):
-        x = 0
-        while x < self.window_size[0]:
-            pygame.draw.line(
-                canvas,
-                0,
-                (x, self.window_size[1] / 2 - 70 / 2),
-                (x, self.window_size[1] / 2 + 70 / 2),
-                width=1,
-            )
-            x += self.scale
-
-    def _render_vehicles(self, canvas):
-        vehicle_counter = [0 for _ in range(self.n_lanes)]
-
-        for l in self.lane_sequence:
-            i = vehicle_counter[l]
-
-            left = self.start_time[l, i]
-            width = self.end_time[l, i] - left
-
-            height = 50
-            top = self.window_size[1] / 2 - height / 2
-
-            pygame.draw.rect(
-                canvas,
-                self.lane_color[l],
-                pygame.Rect(self.scale * left, top, self.scale * width, height),
-                width=0,
-            )
-
-            pygame.draw.rect(
-                canvas,
-                0,
-                pygame.Rect(self.scale * left, top, self.scale * width, height),
-                width=1,
-            )
-
-            vehicle_counter[l] += 1
-
-    def close(self):
-        if self.window is not None:
-            pygame.display.quit()
-            pygame.quit()
-
