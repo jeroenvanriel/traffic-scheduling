2a7364463149019f885595a3157eb175f1893e43
Jeroen van Riel <jeroenvanriel@outlook.com>
2024-01-10 11:50:28 +0100

    remove pygame dependency from env

--- single-intersection/env/setup.py
+++ single-intersection/env/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="single_intersection_gym",
     version="0.0.1",
-    install_requires=["gymnasium==0.29.1", "pygame==2.5.2"],
+    install_requires=["gymnasium==0.29.1"],
 )
