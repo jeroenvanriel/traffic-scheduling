ea327fcafb248b5ecf765e29a1ba3872409f2d66
Jeroen van Riel <jeroenvanriel@outlook.com>
2024-09-11 11:33:40 +0200

    move network-editor out of project
    Moved into its own repo using git subtree split command.

--- network-editor/imports/api/networks.js
+++
@@ -1,3 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const NetworksCollection = new Mongo.Collection('networks');
--- network-editor/imports/api/schedules.js
+++
@@ -1,3 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const SchedulesCollection = new Mongo.Collection('schedules', { 'idGeneration': 'MONGO' });
