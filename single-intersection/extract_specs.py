974b04d923d280c74e6e2eaed7e1663ab67a258f
Jeroen van Riel <jeroenvanriel@outlook.com>
2024-10-17 17:56:05 +0200

    remove single-intersection dir

--- single-intersection/extract_specs.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-specs = pd.read_csv(snakemake.input[0])
-
-for i, spec in specs.iterrows():
-    # save to .npz file
-    filename = snakemake.output[i]
-    np.savez(filename, **spec)
