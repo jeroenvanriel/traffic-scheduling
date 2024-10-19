8da1227922c2f05c2e3e69c1a8922211bc7241d0
Jeroen van Riel <jeroenvanriel@outlook.com>
2024-10-19 15:31:07 +0200

    remove old simulator/automaton

--- simulator/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="single_intersection_gym",
-    version="0.0.1",
-    install_requires=["gymnasium==0.29.1"],
-)
--- simulator/single_intersection_gym/__init__.py
+++
@@ -1,8 +0,0 @@
-from gymnasium.envs.registration import register
-from .env import SingleIntersectionEnv
-
-register(
-     id="SingleIntersectionEnv",
-     entry_point="single_intersection_gym:SingleIntersectionEnv",
-     max_episode_steps=300,
-)
