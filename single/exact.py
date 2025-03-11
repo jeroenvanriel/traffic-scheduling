99bed07018ba071e1947563499bd02a79e990732
Jeroen van Riel <jeroenvanriel@outlook.com>
2025-03-11 16:24:51 +0100

    implement all three cutting planes

--- single/exact.py
+++ single/exact.py
@@ -4,13 +4,14 @@ from itertools import product, combinations
 import os
 
 
-def solve(instance, gap=0.0, timelimit=0, consolelog=False, logfile=None, cutting_planes=0):
+def solve(instance, gap=0.0, timelimit=0, consolelog=False, logfile=None, cutting_planes=None):
     """Solve a single intersection scheduling problem as a MILP.
 
-    cutting_planes:
-    0 - no
-    1 - type 1
-    2 - type 2
+    `cutting_planes` is a list/set specifying which cutting planes to add,
+    possible choices are integers:
+        1 - transitive cutting planes
+        2 - necessary conjunctive cutting planes
+        3 - necessary disjunctive cutting planes
     """
 
     env = gp.Env(empty=True)
@@ -26,12 +27,15 @@ def solve(instance, gap=0.0, timelimit=0, consolelog=False, logfile=None, cuttin
     env.start()
     g = gp.Model(env=env)
 
-    release = instance['release']
-    length = instance['length']
-    switch = instance['switch']  # switch-over time
+    if cutting_planes is None:
+        cutting_planes = []
 
-    N = len(release) # number of lanes
-    n = [len(r) for r in release] # number of arrivals per lane
+    release = instance['release'] # earliest crossing times (a_i)
+    length = instance['length']   # vehicle follow time (rho)
+    switch = instance['switch']   # switch-over time (sigma)
+
+    R = len(release) # number of routes
+    n = [len(r) for r in release] # number of arrivals per route
 
     # big-M
     M = 1000
@@ -40,46 +44,53 @@ def solve(instance, gap=0.0, timelimit=0, consolelog=False, logfile=None, cuttin
 
     # non-negative starting times
     y = {}
-    beta = {}
-    for k in range(N):
-        for j in range(n[k]):
-            y[k, j] = g.addVar(obj=1, vtype=gp.GRB.CONTINUOUS, name=f"y_{k}_{j}")
-            g.addConstr(y[k, j] >= release[k][j])
+    for r in range(R):
+        for k in range(n[r]):
+            y[r, k] = g.addVar(obj=1, vtype=gp.GRB.CONTINUOUS, name=f"y_{r}_{k}")
+            g.addConstr(y[r, k] >= release[r][k])
 
     ### Constraints
 
+    # indicator variables for the necessary cutting planes
+    delta = {}
+
     # conjunctions
-    for k in range(N):
-        for j in range(n[k] - 1):
-            g.addConstr(y[k, j] + length[k][j] <= y[k, j + 1])
+    for r in range(R):
+        for k in range(n[r] - 1):
+            g.addConstr(y[r, k] + length[r][k] <= y[r, k + 1])
 
-            # exhaustive rule constraints
-            if cutting_planes > 0:
-                beta[k, j] = g.addVar(obj=0, vtype=gp.GRB.BINARY, name=f"beta_{k}_{j}")
+            # definition of delta
+            if 2 in cutting_planes or 3 in cutting_planes:
+                delta[r, k] = g.addVar(obj=0, vtype=gp.GRB.BINARY, name=f"delta_{r}_{k}")
+                g.addConstr(y[r, k] + length[r][k] <= release[r][k + 1] + delta[r, k] * M)
+                g.addConstr(y[r, k] + length[r][k] >= release[r][k + 1] - (1 - delta[r, k]) * M)
 
-                g.addConstr(y[k, j] + length[k][j] <= release[k][j + 1] + beta[k, j] * M)
-                g.addConstr(y[k, j] + length[k][j] >= release[k][j + 1] - (1 - beta[k, j]) * M)
-
-                g.addConstr(y[k, j] + length[k][j] >= y[k, j + 1] - (1 - beta[k, j]) * M)
+            if 2 in cutting_planes:
+                g.addConstr(y[r, k] + length[r][k] >= y[r, k + 1] - (1 - delta[r, k]) * M)
 
 
     # disjunctions
     o = {}
-    for k1, k2 in combinations(range(N), 2):
-        for j, l in product(range(n[k1]), range(n[k2])):
-            oc = g.addVar(obj=0, vtype=gp.GRB.BINARY, name=f"o_{k1}_{j}_{k2}_{l}")
-            o[k1, j, k2, l] = oc
+    for r1, r2 in combinations(range(R), 2):
+        for k, l in product(range(n[r1]), range(n[r2])):
+            oc = g.addVar(obj=0, vtype=gp.GRB.BINARY, name=f"o_{r1}_{k}_{r2}_{l}")
+            o[r1, k, r2, l] = oc
 
-            g.addConstr(y[k1, j] + length[k1][j] + switch <= y[k2, l] + oc * M)
-            g.addConstr(y[k2, l] + length[k2][l] + switch <= y[k1, j] + (1 - oc) * M)
+            g.addConstr(y[r1, k] + length[r1][k] + switch <= y[r2, l] + oc * M)
+            g.addConstr(y[r2, l] + length[r2][l] + switch <= y[r1, k] + (1 - oc) * M)
 
-    if cutting_planes == 2:
-        for k, k2 in combinations(range(N), 2):
-            for j in range(n[k] - 1): # for all conjunctive pairs (j, j + 1)
-                for j2 in range(n[k2]):
-                    g.addConstr(beta[k, j] + (1 - o[k, j, k2, j2]) + o[k, j + 1, k2, j2] <= 2)
-                    g.addConstr(beta[k, j] + o[k, j, k2, j2] + (1 - o[k, j + 1, k2, j2]) <= 2)
+            # transitive cutting planes
+            if 1 in cutting_planes:
+                g.addConstr(gp.quicksum(o[r1, p, r2, q] for p in range(0, k) for q in range(l + 1, n[r2])) <= o[r1, k, r2, l] * M)
 
+
+    # necessary disjunctive cutting planes
+    if 3 in cutting_planes:
+        for r1, r2 in combinations(range(R), 2):
+            for l in range(n[r1] - 1): # for all conjunctive pairs (i, j) = ((r1, l), (r1, l + 1))
+                for k in range(n[r2]):
+                    g.addConstr(delta[r1, l] + (1 - o[r1, l, r2, k]) + o[r1, l + 1, r2, k] <= 2)
+                    g.addConstr(delta[r1, l] + o[r1, l, r2, k] + (1 - o[r1, l + 1, r2, k]) <= 2)
 
     ### Solving
 
@@ -89,8 +100,12 @@ def solve(instance, gap=0.0, timelimit=0, consolelog=False, logfile=None, cuttin
     g.optimize()
 
     res = { 'y': [], 'obj': g.getObjective().getValue() }
-    y = { k : (v.X if hasattr(v, 'X') else v) for k, v in y.items() }
-    for k in range(N):
-        res['y'].append(np.array([y[k, j] for j in range(n[k])]))
+    y = { r : (v.X if hasattr(v, 'X') else v) for r, v in y.items() }
+    for r in range(R):
+        res['y'].append(np.array([y[r, j] for j in range(n[r])]))
+
+    res['done'] = int(g.status == gp.GRB.OPTIMAL)
+    res['gap'] = g.MIPGap
+    res['time'] = g.Runtime
 
     return res
